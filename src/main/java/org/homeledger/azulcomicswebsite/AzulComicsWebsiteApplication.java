754b75a85cea301ca9860cf773a620d94892a267
Teo <144364977+MatRodriguezPa@users.noreply.github.com>
2024-08-12 00:40:19 -0500

    changed to spring

---
+++ src/main/java/org/homeledger/azulcomicswebsite/AzulComicsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.homeledger.azulcomicswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzulComicsWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzulComicsWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/homeledger/azulcomicswebsite/AzulComicsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.homeledger.azulcomicswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzulComicsWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
