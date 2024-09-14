665eea724ac7a746c41f9a4dcf3978106cd666b2
Teo <matrodriguezpa@unal.edu.co>
2024-09-14 15:05:36 -0500

    project architecture

---
+++ core/src/main/java/com/matrodriguezpa/azulcomics/AzulComicsApplication.java
@@ -0,0 +1,13 @@
+package com.matrodriguezpa.azulcomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzulComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzulComicsApplication.class, args);
+	}
+
+}
